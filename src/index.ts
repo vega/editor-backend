b5347ca0f883f24a5da172243386695a36fe77f4
Aman Sharma <mannu.poski10@gmail.com>
2019-05-18 03:30:07 +0530

    Setup basic Node project with Express and TypeScript (#2)
    * Setup basic Node project with Express and TypeScript
    
    * Integrate ESLint instead of TSLint for formatting

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+const express = require('express')
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+const app = express()
+const port = process.env.PORT || 9000
+app.get('/', (req, res) => {
+  res.send('Initialized backend server for vega/editor-backend')
+})
+app.listen(port, err => {
+  if (err) {
+    return console.error(err)
+  }
+  return console.log(`Server is listening on ${port}`)
+})
