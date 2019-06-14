c2c9aba1d3fa7e5e54e4682b6d66706836b4d861
Aman Sharma <mannu.poski10@gmail.com>
2019-06-14 17:53:46 +0530

    Configure session based authentication

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import session from 'express-session'
 import passport from 'passport'
+import cors from 'cors'
 
 import { sessionSecret } from '../config/index'
 
@@ -12,7 +13,7 @@ import AuthController from './controllers/auth'
  * Configuration of the express application
  */
 class App {
-  
+
   public app: express.Application
 
   /**
@@ -28,13 +29,13 @@ class App {
 
   /**
    * Initializes middleware for accessing request and response objects
-   * 
+   *
    * @private
    */
   private initializeMiddleWares() {
     // Configuration for creating session cookies
     this.app.use(session({
-      key: 'user',
+      key: 'vegasessid',
       secret: sessionSecret,
       resave: false,
       saveUninitialized: true,
@@ -42,13 +43,29 @@ class App {
       cookie: { httpOnly: false, maxAge: 1 * 24 * 60 * 60 * 1000 },
     }))
 
+    // Arrays of domains allowed
+    const whitelist = [
+      'http://localhost:8081',
+      'http://localhost:8080',
+    ]
+    const corsOptions = {
+      origin: (origin, callback) => {
+        if (whitelist.indexOf(origin) !== -1 || !origin) {
+          callback(null, true)
+        } else {
+          callback(new Error('Not allowed by CORS'))
+        }
+      },
+      credentials: true,
+    }
+    this.app.use(cors(corsOptions))
     this.app.use(passport.initialize())
     this.app.use(passport.session())
   }
 
   /**
    * Sets routes for each controller
-   * 
+   *
    * @param {Controller} controllers Array of controllers
    */
   private initializeControllers(controllers: Controller[]) {
@@ -56,7 +73,6 @@ class App {
       this.app.use('/', controller.router)
     })
   }
-
 }
 
 /**
